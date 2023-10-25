ede826adec5563ff60e08383bb3c33d5e343a6c5
bruno22213 <brunademais2012@hotmail.com>
2023-10-25 13:44:55 -0300

    meu primeiro commit

---
+++ variavel.js
@@ -0,0 +1,7 @@
+let poteCafe = "café pilao"
+let poteAcucar = "açucar cristal"
+let poteBiscoito =" biscoito de maizena"
+
+console.log("na cozinha da vó tem:" + poteAcucar + "-" + poteBiscoito  + "-"
++ poteCafe
+)
